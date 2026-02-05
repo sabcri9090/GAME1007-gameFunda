64f613a890e7ad7abfde4942cc9a569119567d84
sabcri9090 <sabogalcris6@gmail.com>
2026-02-04 22:20:24 -0500

    exercise2 finished
    I was stuck on fixing errors and how VSC was not cooperating

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿//this is the begining code, to welcome users, and their choosing of 5 operators
+Console.WriteLine("hello, let me calc 4 u");
+Console.WriteLine("what operator do you want: +, -, *, /, %. which? ");
+string userChoice = Console.ReadLine();
+
+//answering
+
+Console.WriteLine("enter a number");
+string answer = Console.ReadLine();
+int number1 = int.Parse(answer);
+Console.WriteLine("enter your number");
+answer = Console.ReadLine();
+int number2 = int.Parse(answer);
+
+//if statements
+
+if(userChoice == "+")
+{
+    int result = number1 + number2;
+    Console.WriteLine($"{number1} + {number2} = {result}");
+}
+
+else if(userChoice == "-")
+{
+    int result = number1 - number2;
+    Console.WriteLine($"{number1} - {number2} = {result}");
+}
+
+else if(userChoice == "*")
+{
+    int result = number1 * number2;
+    Console.WriteLine($"{number1} * {number2} = {result}");
+}
+
+else if(userChoice == "/")
+{
+    int result = number1 / number2;
+    Console.WriteLine($"{number1} / {number2} = {result}");
+}
+
+else if(userChoice == "%")
+{
+    int result = number1 % number2;
+    Console.WriteLine($"{number1} % {number2} = {result}");
+}
+else
+{
+    Console.WriteLine("not a valid operator");
+}
+
